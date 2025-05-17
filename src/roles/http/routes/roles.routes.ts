beeec54478920a497d1d1b3ff440852e4837ca67
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-17 00:02:58 -0300

    Create usecase showrole

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,17 +1,20 @@
-import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { createRolesController } from '@roles/useCases/createRole'
 import { listRolesController } from '@roles/useCases/listRoles'
+import { showRolesController } from '@roles/useCases/showRole'
 import { Router } from 'express'
 
 const rolesRouter = Router()
-const rolesRepository = new RolesRepository()
 
 rolesRouter.post('/', (request, response) => {
-  return createRolesController.handle(request, response)
+  return listRolesController.handle(request, response)
 })
 
 rolesRouter.get('/', (request, response) => {
   return listRolesController.handle(request, response)
+})
+
+rolesRouter.get('/:id', (request, response) => {
+  return showRolesController.handle(request, response)
 })
 
 export { rolesRouter }
