f57cd2a62a940e0c036afa288385b1c5d4c54821
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-17 02:37:29 -0300

    Add data validation - roles

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -4,26 +4,61 @@ import { listRolesController } from '@roles/useCases/listRoles'
 import { showRolesController } from '@roles/useCases/showRole'
 import { updateRolesController } from '@roles/useCases/updateRole'
 import { Router } from 'express'
+import { celebrate, Joi, Segments} from 'celebrate'
 
 const rolesRouter = Router()
 
-rolesRouter.post('/', (request, response) => {
+rolesRouter.post(
+  '/',
+  celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+  })
+}),
+(request, response) => {
+  return createRolesController.handle(request, response)
+})
+
+rolesRouter.get(
+  '/',
+  celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+    limit: Joi.number(),
+  })
+}), (request, response) => {
   return listRolesController.handle(request, response)
 })
 
-rolesRouter.get('/', (request, response) => {
-  return listRolesController.handle(request, response)
-})
-
-rolesRouter.get('/:id', (request, response) => {
+rolesRouter.get(
+  '/:id',
+  celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().uuid().required(),
+  })
+}), (request, response) => {
   return showRolesController.handle(request, response)
 })
 
-rolesRouter.put('/:id', (request, response) => {
+rolesRouter.put(
+  '/:id',
+  celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().uuid().required(),
+  }),
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+  })
+}), (request, response) => {
   return updateRolesController.handle(request, response)
 })
 
-rolesRouter.delete('/:id', (request, response) => {
+rolesRouter.delete('/:id',
+  celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().uuid().required(),
+  })
+}), (request, response) => {
   return deleteRolesController.handle(request, response)
 })
 
