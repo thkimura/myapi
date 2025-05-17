808363e33ea2d262052d2c9ba5819bb494da0dee
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-16 23:45:18 -0300

    Refactor usecases

--- src/roles/useCases/listRoles/ListRolesUseCase.ts
+++ src/roles/useCases/listRoles/ListRolesUseCase.ts
@@ -1,10 +1,20 @@
-import { Role } from "@roles/http/routes/entities/Role";
-import { RolesRepository } from "@roles/repositories/RolesRepository";
 
+import { RolesPaginateProperties, RolesRepository } from "@roles/repositories/RolesRepository";
+
+type ListRolesUseCaseRequest = {
+  page: number
+  limit: number
+}
 export class ListRolesUseCase {
   constructor(private rolesRepository: RolesRepository) {}
 
-  execute(): Role[] {
-    return this.rolesRepository.findAll()
+  execute( {limit, page}: ListRolesUseCaseRequest): Promise<RolesPaginateProperties> {
+    const take = limit
+    const skip = (Number(page) - 1) * take
+    return this.rolesRepository.findAll({
+      page,
+      take,
+      skip,
+    })
   }
 }
