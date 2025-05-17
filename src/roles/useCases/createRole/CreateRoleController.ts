381cc67b2732214806490c77cf954d0d0dd50362
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-17 15:54:27 -0300

    Refactor roles routes

--- src/roles/useCases/createRole/CreateRoleController.ts
+++ src/roles/useCases/createRole/CreateRoleController.ts
@@ -3,12 +3,12 @@ import { container } from 'tsyringe'
 import { CreateRoleUseCase } from './CreateRoleUseCase'
 
 export class CreateRoleController {
-  constructor(private createRoleUseCase: CreateRoleUseCase ) {}
+
 
   async handle(request: Request, response: Response): Promise<Response> {
     const createRoleUseCase = container.resolve(CreateRoleUseCase)
     const { name } = request.body
-    const role = await this.createRoleUseCase.execute({ name })
+    const role = await createRoleUseCase.execute({ name })
 
   return response.status(201).json(role)
   }
