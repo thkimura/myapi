f1cb76d17b424c6e977197e5ea4a95732b920332
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-18 14:47:17 -0300

    Create user entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -3,10 +3,11 @@ import { CreateRolesTable1722717797984 } from "./migrations/1722717797984-Create
 import { Role } from "@roles/http/routes/entities/Role"
 import { CreateUsersTable1747509157491 } from "./migrations/1747509157491-CreateUsersTable"
 import { AddRoleIdToUsersTable1747509658497 } from "./migrations/1747509658497-AddRoleIdToUsersTable"
+import { User } from "@users/entities/users"
 
 export const dataSource = new DataSource({
     type: "sqlite",
     database: "./db.sqlite",
-    entities: [Role],
+    entities: [Role, User],
     migrations: [CreateRolesTable1722717797984, CreateUsersTable1747509157491, AddRoleIdToUsersTable1747509658497],
 })
