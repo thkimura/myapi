28bdc56cecf3be84f71bb59591adcbba9c28d4aa
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-28 19:15:05 -0300

    Create user repository

---
+++ src/users/repositories/UsersRepository.ts
@@ -0,0 +1,60 @@
+import { User } from "@users/entities/Users";
+import { CreateUserDTO, IUsersRepository, PaginateParams, UsersPaginateProperties } from "./IUsersRepository";
+import { Repository } from "typeorm";
+import { dataSource } from "@shared/typeorm";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(User);
+  }
+
+  async create({ name, email, password, isAdmin, role }: CreateUserDTO): Promise<User> {
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      isAdmin,
+      role,
+    });
+    return this.repository.save(user);
+  }
+
+  async save(user: User): Promise<User> {
+    return this.repository.save(user);
+  }
+
+  async findAll({ page, skip, take }: PaginateParams): Promise<UsersPaginateProperties> {
+    const [users, count] = await this.repository
+    .createQueryBuilder('r')
+    .leftJoinAndSelect('r.role', 'role')
+    .skip(skip)
+    .take(take)
+    .getManyAndCount();
+  const result = {
+    per_page: take,
+    total: count,
+    current_page: page,
+    data: users,
+  };
+  return result;
+
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.repository.findOneBy({ id })
+  }
+
+  async findByName(name: string): Promise<User | null> {
+    return this.repository.findOne({ where: { name } });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.repository.findOneBy({ email });
+  }
+
+  async delete(user: User): Promise<void> {
+    await this.repository.remove(user);
+  }
+}
