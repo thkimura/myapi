f019c16a7559c247f98ba6c810cb5f2dd4014e75
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2025-05-30 18:53:49 -0300

    Add CreateUser route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { rolesRouter } from '@roles/http/routes/roles.routes'
+import { usersRouter } from '@users/http/users.routes'
 
 const routes = Router()
 
@@ -7,4 +8,5 @@ routes.get('/', (request, response) => {
   return response.json({ message: 'Ola Dev!' })
 })
 routes.use('/roles',rolesRouter)
+routes.use('/users',usersRouter)
 export { routes }
--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,5 +1,8 @@
 import { IUsersRepository } from "@users/repositories/IUsersRepository";
 import { UsersRepository } from "@users/repositories/UsersRepository";
+import { CreateUserUseCase } from "@users/useCases/createUser/CreateUserUseCase";
 import { container } from "tsyringe";
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
+
+container.registerSingleton('CreateUserUseCase', CreateUserUseCase)
